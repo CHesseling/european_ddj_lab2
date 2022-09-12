7c8e2dcd02e0637a8d842921f92f9dec824173c6
Claus Hesseling <c.hesseling.fm@ndr.de>
2022-09-12 11:38:20 +0200

    This is the first Pandas file
    bla bla

---
+++ first_script.py
@@ -0,0 +1,10 @@
+#%%
+
+import pandas as pd
+
+#%%
+
+df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vThd-GGLexzvM-fq0CpEPcABELvP-UBce5lEqpC9MwFtjYrTvYyfDm89UBST2wYzIuz835cDShOK72A/pub?gid=204995029&single=true&output=csv')
+
+#%%
+df
